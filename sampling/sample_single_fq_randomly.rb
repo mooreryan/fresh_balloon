9c3aed47ae96de1d1e42c5e8e0656b18097b5d9a
Ryan Moore <moorer@udel.edu>
2016-04-07 19:35:56 -0400

    Add license

--- sampling/sample_single_fq_randomly.rb
+++ sampling/sample_single_fq_randomly.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/env ruby
 
+# Copyright 2016 Ryan Moore
+# Contact: moorer@udel.edu
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see
+# <http://www.gnu.org/licenses/>.
+
 def count_seqs_fast fname
   if fname.match(/.gz$/)
     num_seqs = (`gunzip -c #{fname} | wc -l`.to_f / 4).round
@@ -8,7 +25,7 @@ def count_seqs_fast fname
   end
 
   num_seqs
-dend
+end
 
 require "parse_fasta"
 require "abort_if"
