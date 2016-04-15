67563288c78ba51628a39094ac1b61d28880a3d0
Ryan Moore <moorer@udel.edu>
2016-04-15 15:56:55 -0400

    Add script

---
+++ annotate_seqs.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+require "parse_fasta"
+
+def clean str
+  str.gsub(/[^\p{Alnum}_]+/, "_").gsub(/_+/, "_")
+end
+
+ARGV.each do |fname|
+  FastaFile.open(fname, "rt").each_record do |head, seq|
+    puts "#{clean(fname)}_#{head}"
+  end
+end
