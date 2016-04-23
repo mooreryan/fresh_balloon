c53292f746b952dd1ca54a1d51a4d5966c9b1422
Ryan Moore <moorer@udel.edu>
2016-04-23 09:51:55 -0400

    Update

--- get_conitgs_given_rpsblast.rb
+++ get_conitgs_given_rpsblast.rb
@@ -4,7 +4,9 @@ blast_f = ARGV[0]
 contigs_f = ARGV[1]
 
 names = []
+n = 0
 File.open(blast_f).each_line do |line|
+  n += 1; $stderr.printf("Reading blast: %d\r", n) if (n % 10_000).zero?
   orfname, *rest = line.chomp.split "\t"
 
   orfmatch = orfname.match(/(.*)_[0-9]+_[0-9]+_[0-9]+$/)
@@ -15,6 +17,7 @@ File.open(blast_f).each_line do |line|
 end
 
 FastaFile.open(contigs_f).each_record_fast do |head, seq|
+  n += 1; $stderr.printf("Reading fasta: %d\r", n) if (n % 10_000).zero?
   if orfs.any? { |orf| orf == head }
     puts ">#{head}\n#{seq}"
   end
