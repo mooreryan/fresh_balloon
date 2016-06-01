ef26e50c5c84dff3534d45d5e7ab7e9a99c04057
Ryan Moore <moorer@udel.edu>
2016-06-01 17:31:20 -0400

    Change to 1-based index

--- column_math.rb
+++ column_math.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/env ruby
 
-VERSION = "0.1.1"
+VERSION = "0.2.0"
 
 Signal.trap("PIPE", "EXIT")
 
 if ARGV.count < 4
   warn "VERSION: v#{VERSION}"
-  abort "USAGE: column_math.rb fname.txt operation column1 column2 ..."
+  warn "NOTE: columns are 1 indexed"
+  abort "USAGE: column_math.rb fname.txt 'operation' column1 column2 ..."
 end
 
 fname = ARGV.shift
@@ -20,7 +21,7 @@ warn "Columns requested: #{columns.inspect}"
 File.open(fname).each_line do |line|
   ary = line.chomp.split "\t"
 
-  result = columns.map { |colnum| ary[colnum.to_i].to_f }.reduce(op.to_sym)
+  result = columns.map { |colnum| ary[colnum.to_i-1].to_f }.reduce(op.to_sym)
 
   puts [ary, result].join "\t"
 end
