ecf9ad02c42180916284c66f687aa4d1fa301680
Ryan Moore <moorer@udel.edu>
2016-06-24 16:42:09 -0400

    Update

--- usearch_to_biom.rb
+++ usearch_to_biom.rb
@@ -8,6 +8,7 @@
 # records.
 
 require "abort_if"
+require "parse_fasta"
 
 include AbortIf
 
@@ -17,12 +18,15 @@ end
 
 Signal.trap("PIPE", "EXIT")
 
-abort_if ARGV.length < 2,
-         "USAGE -- usearch_to_biom.rb clusters.uc sample1_name " +
+abort_if ARGV.length < 3,
+         "USAGE -- usearch_to_biom.rb clusters.uc centroids.fa sample1_name " +
          "sample2_name ..."
 
 uc_f = ARGV.shift
+centroids_f = ARGV.shift
 samples = ARGV
+
+centroids = FastaFile.open(centroids_f, "rt").to_hash
 
 # assumes that the first thing when split on _ is the sample name
 def get_sample name
@@ -30,12 +34,15 @@ def get_sample name
 end
 
 clusters = {}
+centroid_to_cluster = {}
 File.open(uc_f, "rt").each_line do |line|
   ary = line.chomp.split "\t"
 
   type = ary[0]
   cluster_num = ary[1]
   seq_name = ary[8]
+
+  centroid_to_cluster[seq_name] = cluster_num if type == "C"
 
   if type == "C" || type == "H"
     if clusters.has_key? cluster_num
@@ -49,12 +56,24 @@ end
 largest_cluster_name_size =
   clusters.keys.sort_by(&:length).reverse.first.length
 
-puts ["#OTU ID", samples].flatten.join "\t"
-clusters.each do |cluster, counts|
-  per_sample_counts = samples.map do |sample|
-    counts.count sample
-  end
+biom_f = uc_f + ".biom.tsv"
+File.open(biom_f, "w") do |f|
+  f.puts ["#OTU ID", samples].flatten.join "\t"
+  clusters.each do |cluster, counts|
+    per_sample_counts = samples.map do |sample|
+      counts.count sample
+    end
 
-  cluster_name = leftpad cluster, "0", largest_cluster_name_size
-  puts ["Otu#{cluster_name}", per_sample_counts].flatten.join "\t"
+    cluster_name = leftpad cluster, "0", largest_cluster_name_size
+    f.puts ["Otu#{cluster_name}", per_sample_counts].flatten.join "\t"
+  end
+end
+
+renamed_fasta = centroids_f + ".otu_names.fa"
+File.open(renamed_fasta, "w") do |f|
+  centroids.each do |name, seq|
+    new_name = "Otu#{leftpad(centroid_to_cluster[name], "0", largest_cluster_name_size)}"
+
+    f.puts ">#{new_name}\n#{seq}"
+  end
 end
